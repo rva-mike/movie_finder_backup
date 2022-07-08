4cb4479393ff329c8878af0645ed73145a0223f8
Mike <mike.levy.rva@gmail.com>
2022-07-08 12:16:24 -0400

    inital backup

---
+++ assets/homepage.js
@@ -0,0 +1,54 @@
+var inputEl = document.querySelector(".input")
+var generateBtnEl = document.querySelector(".generate")
+var  saveBtnEl = document.querySelector(".save")
+var savedMoviesUlEl = document.querySelector(".savedMovies")
+
+
+function randomUrl() {
+
+    var page = Math.floor(Math.random() * 466)
+
+    return 'https://api.themoviedb.org/3/movie/top_rated?api_key=57a87d855b4da05e62b4da62e5c0856e&language=en-US&page=' + page + '&region=US'
+}
+
+var fetchFunction = function(){
+
+    fetch(randomUrl())
+    .then(response => response.json())
+    .then(response => {
+        console.log(response.results[0])
+        var movie = randomIndex(response.results)
+            inputEl.innerHTML = response.results[movie].title
+    })
+    .catch(err => console.error(err));
+}
+
+// btnEl.addEventListener("click", fetchFunction){
+//     document.createElement(li)
+// }
+
+
+generateBtnEl.addEventListener("click", function(){
+    fetchFunction();
+    inputEl.classList.remove("opacity")
+
+
+})
+
+
+
+function randomIndex(array) {
+
+    return Math.floor(Math.random() * array.length)
+}
+
+saveBtnEl.addEventListener("click", function(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(inputEl.innerHTML));
+    savedMoviesUlEl.appendChild(li);
+
+
+
+})
+
+
